b26e626933aa59aeaadf69b802c79464eee7fd7f
Domaman202 <vip.domaman@mail.ru>
2021-03-06 10:34:23 +0300

    i stupid

--- src/main/java/ru/DmN/HWYLAFF/mixin/MixinGuiConfigWaila.java
+++ src/main/java/ru/DmN/HWYLAFF/mixin/MixinGuiConfigWaila.java
@@ -1,26 +1,19 @@
 package ru.DmN.HWYLAFF.mixin;
 
 import mcp.mobius.waila.Waila;
-import mcp.mobius.waila.api.impl.config.WailaConfig;
 import mcp.mobius.waila.gui.GuiConfigWaila;
 import mcp.mobius.waila.gui.GuiOptions;
 import mcp.mobius.waila.gui.config.OptionsEntryButton;
 import mcp.mobius.waila.gui.config.OptionsListWidget;
-import mcp.mobius.waila.gui.config.value.OptionsEntryValueBoolean;
-import mcp.mobius.waila.gui.config.value.OptionsEntryValueCycle;
-import mcp.mobius.waila.gui.config.value.OptionsEntryValueEnum;
-import mcp.mobius.waila.gui.config.value.OptionsEntryValueInput;
 import mcp.mobius.waila.utils.JsonConfig;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
-import org.apache.commons.lang3.StringEscapeUtils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import ru.DmN.HWYLAFF.classes.Anon2GuiOptions;
 import ru.DmN.HWYLAFF.classes.Anon3GuiOptions;
 import ru.DmN.HWYLAFF.classes.Anon4GuiOptions;
@@ -32,6 +25,11 @@ public abstract class MixinGuiConfigWaila extends GuiOptions {
     /** Not Used */
     public MixinGuiConfigWaila() { super(null, null); }
 
+    /**
+     * @author DomamaN202
+     * @reason Fix
+     */
+    @Overwrite
     public OptionsListWidget getOptions() {
         MinecraftClient var10003 = this.client;
         int var10004 = this.width + 45;
@@ -40,15 +38,9 @@ public abstract class MixinGuiConfigWaila extends GuiOptions {
         JsonConfig var10009 = Waila.CONFIG;
         Objects.requireNonNull(var10009);
         OptionsListWidget options = new OptionsListWidget(this, var10003, var10004, var10005, 32, var10007, 30, var10009::save);
-        options.add(new OptionsEntryButton(Util.createTranslationKey("config", new Identifier("waila", "general")), new ButtonWidget(0, 0, 100, 20, new LiteralText(""), (w) -> {
-            this.client.openScreen(new Anon2GuiOptions(this, new TranslatableText(Util.createTranslationKey("config", new Identifier("waila", "general")))));
-        })));
-        options.add(new OptionsEntryButton(Util.createTranslationKey("config", new Identifier("waila", "overlay")), new ButtonWidget(0, 0, 100, 20, new LiteralText(""), (w) -> {
-            this.client.openScreen(new Anon3GuiOptions(this, new TranslatableText(Util.createTranslationKey("config", new Identifier("waila", "overlay"))), (GuiConfigWaila) (Object) this));
-        })));
-        options.add(new OptionsEntryButton(Util.createTranslationKey("config", new Identifier("waila", "formatting")), new ButtonWidget(0, 0, 100, 20, new LiteralText(""), (w) -> {
-            this.client.openScreen(new Anon4GuiOptions(this, new TranslatableText(Util.createTranslationKey("config", new Identifier("waila", "overlay")))));
-        })));
+        options.add(new OptionsEntryButton(Util.createTranslationKey("config", new Identifier("waila", "general")), new ButtonWidget(0, 0, 100, 20, new LiteralText(""), (w) -> this.client.openScreen(new Anon2GuiOptions(this, new TranslatableText(Util.createTranslationKey("config", new Identifier("waila", "general"))))))));
+        options.add(new OptionsEntryButton(Util.createTranslationKey("config", new Identifier("waila", "overlay")), new ButtonWidget(0, 0, 100, 20, new LiteralText(""), (w) -> this.client.openScreen(new Anon3GuiOptions(this, new TranslatableText(Util.createTranslationKey("config", new Identifier("waila", "overlay"))), (GuiConfigWaila) (Object) this)))));
+        options.add(new OptionsEntryButton(Util.createTranslationKey("config", new Identifier("waila", "formatting")), new ButtonWidget(0, 0, 100, 20, new LiteralText(""), (w) -> this.client.openScreen(new Anon4GuiOptions(this, new TranslatableText(Util.createTranslationKey("config", new Identifier("waila", "overlay"))))))));
         return options;
     }
 }
